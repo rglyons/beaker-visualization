6902ce7fde57b88d31d829cad4dde38f26e1e657
Robert Lyons <rlyons@apm.com>
2017-09-21 10:38:56 -0700

    Express app init

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Beaker Visualzation application listening on port 3000')
+})
